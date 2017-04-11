362f9366649fbc5e999c99734b8d8eefa9a10d79
426111 <balasubramania.sharmav@cognizant.com>
2017-04-11 11:55:39 +0530

    Branch commit

---
+++ tests/spec.js
@@ -0,0 +1,22 @@
+module.exports = {  
+  
+  tags: ['blockchain','bcts','trade','supply chain finance','decentralized app'],
+
+  before: function(browser) {
+      console.log("Setting up...");
+      browser.url("http://localhost:3000");
+    },
+
+    after : function(browser) {
+      browser.end()
+      console.log("Closing down...");
+    },
+
+    'body visible' : function (client) {
+      client.waitForElementVisible('body',2000);
+    },
+    
+    'h1 visible' : function (client) {
+      client.waitForElementVisible('h1',2000);
+    }
+}
