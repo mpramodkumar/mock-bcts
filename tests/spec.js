2d35e3a73d8d75a9bd50ebfa3c66fd1b507867b4
426111 <balasubramania.sharmav@cognizant.com>
2017-04-14 15:14:42 +0530

    spec changes for implementation

--- tests/spec.js
+++ tests/spec.js
@@ -1,35 +1,47 @@
 module.exports = {
+  tags: [
+    'blockchain',
+    'bcts',
+    'trade',
+    'supply chain finance',
+    'decentralized app',
+  ],
 
-  tags: ['blockchain', 'bcts', 'trade', 'supply chain finance', 'decentralized app'],
-
-  before: function (browser) {
-    console.log("Setting up...");
-    browser.url("http://localhost:3000");
+  before: function(browser) {
+    console.log('Setting up...');
+    browser.url('http://localhost:3000');
   },
 
-  after: function (browser) {
-    browser.end()
-    console.log("Closing down...");
+  after: function(browser) {
+    browser.end();
+    console.log('Closing down...');
   },
 
-  'body visible': function (client) {
+  'body visible': function(client) {
     client.waitForElementVisible('body', 5000);
   },
 
-  'has all the required links in left nav': function (client) {
-    var reqNavLinks = new Array("Overview",
-                                "Industry Players",
-                                "Competitive Analysis",
-                                "Industry Articles",
-                                "Technical Articles",
-                                "Glossary",
-                                "Code Spikes",
-                                "Implementation"
-                                );
-    
-    reqNavLinks.forEach(function (navLink) {                               
-      client.expect.element(navLink, 'link text').to.be.present;
-    }); 
-  }
+  'has all the required links in left nav': function(client) {
+    var reqNavLinks = new Array(
+      'Overview',
+      'Industry Players',
+      'Competitive Analysis',
+      'Industry Articles',
+      'Technical Articles',
+      'Glossary',
+      'Code Spikes',
+      'Implementation'
+    );
 
-}
+    reqNavLinks.forEach(function(navLink) {
+      client.expect.element(navLink, 'link text').to.be.present;
+    });
+  },
+
+  'Test for Implementation click': function(client) {
+    client.click('link text', 'Implementation');
+    client.expect
+      .element('h2', 'css selector')
+      .text.to.contain('Implementation');
+  }
+};
