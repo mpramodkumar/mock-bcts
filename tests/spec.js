fe5a0966a10bdb1c35e103973f97f0bdd49f26a5
pramod529 <pramod.victory@gmail.com>
2017-04-14 15:56:08 +0530

    code formating changes made

--- tests/spec.js
+++ tests/spec.js
@@ -2,40 +2,40 @@ module.exports = {
 
   tags: ['blockchain', 'bcts', 'trade', 'supply chain finance', 'decentralized app'],
 
-  before: function (browser) {
+  before: function(browser) {
     console.log("Setting up...");
     browser.url("http://localhost:3000");
   },
 
-  after: function (browser) {
+  after: function(browser) {
     browser.end()
     console.log("Closing down...");
   },
 
-  'body visible': function (client) {
+  'body visible': function(client) {
     client.waitForElementVisible('body', 5000);
   },
 
-  'has all the required links in left nav': function (client) {
+  'has all the required links in left nav': function(client) {
     var reqNavLinks = new Array("Overview",
-                                "Industry Players",
-                                "Competitive Analysis",
-                                "Industry Articles",
-                                "Technical Articles",
-                                "Glossary",
-                                "Code Spikes",
-                                "Implementation"
-                                );
+      "Industry Players",
+      "Competitive Analysis",
+      "Industry Articles",
+      "Technical Articles",
+      "Glossary",
+      "Code Spikes",
+      "Implementation"
+    );
 
-    reqNavLinks.forEach(function (navLink) {
+    reqNavLinks.forEach(function(navLink) {
       client.expect.element(navLink, 'link text').to.be.present;
     });
   },
-  'Test for Technical articles tab': function (client) {
+  'Test for Technical articles tab': function(client) {
     client.expect.element('Technical Articles', 'link text').to.be.present;
   },
 
-  'Test for Technical articles tab click': function (client) {
+  'Test for Technical articles tab click': function(client) {
     client.click('link text', 'Technical Articles');
     client.expect.element('#tab5 h2', 'css selector').text.to.contain('Technical articles');
   }
